c92f021806dc880952ce675329f7992195b12a73
JudeArasu <judearasu@gmail.com>
2013-10-02 11:59:50 +0530

    proj structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweeplus::Application.config.secret_key_base = '49dbf3239a0a88d4a3fc68e21fe3bfa5f2af6a56c70371cf80449f17631fea18c9310f4d9154dda1c5c8e8d028d3b2b051534d92265ae4fa16290e010a0c309a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tweeplus::Application.config.session_store :cookie_store, key: '_tweeplus_session'
