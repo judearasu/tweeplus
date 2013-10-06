0e4781b815c37e68d4115607816f114f36b6b3ed
JudeArasu <judearasu@gmail.com>
2013-10-06 23:21:32 +0530

    stream works with sample tweets

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-Twitter::REST::Client.new do |config|
+@client_rest=Twitter::REST::Client.new do |config|
   config.consumer_key        = 'Oz2823qZe95ZngJverF8rw'
   config.consumer_secret     = 'AQcXRkYX1woJDtlbHUj8xJC3GrGiOHj2jmlDLbTVpc'
   config.access_token        = '27643324-1SEH3ycadfpqOFjketZBmLnPsQYgqzlRbivVY50aL'
@@ -11,3 +11,10 @@ end
 #  config.oauth_token = '27643324-1SEH3ycadfpqOFjketZBmLnPsQYgqzlRbivVY50aL'
 #  config.oauth_token_secret = 'iHDbsPzka9qGNgNRXOyxlmm6heLex8GPUnfvKkkZA'
 #end
+
+@client_stream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "Oz2823qZe95ZngJverF8rw"
+  config.consumer_secret     = "AQcXRkYX1woJDtlbHUj8xJC3GrGiOHj2jmlDLbTVpc"
+  config.access_token        = "27643324-1SEH3ycadfpqOFjketZBmLnPsQYgqzlRbivVY50aL"
+  config.access_token_secret = "iHDbsPzka9qGNgNRXOyxlmm6heLex8GPUnfvKkkZA"
+end
