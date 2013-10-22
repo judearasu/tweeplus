d4504c6e0d9c44e3187b83a513d7cba25e0c052c
JudeArasu <judearasu@gmail.com>
2013-10-22 21:27:07 +0530

    controller added

--- ng-app/app/scripts/app.js
+++ ng-app/app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** app level module which depends on services and controllers */
-angular.module('tweeplus', ['tweeplus.services', 'tweeplus.controllers', 'tweeplus.directives', 'tweeplus.filters']);
+angular.module('tweeplus', ['tweeplus.services', 'tweeplus.controllers']);
 
 /** services module initialization, allows adding services to module in multiple files */
 angular.module('tweeplus.services', []);
