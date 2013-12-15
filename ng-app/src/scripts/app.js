1dd588ba8aa5c5b78293e46cc6cf0808345b9ca2
JudeArasu <judearasu@gmail.com>
2013-12-15 22:18:34 +0530

    bower modified

--- ng-app/src/scripts/app.js
+++ ng-app/src/scripts/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
 /** app level module which depends on services and controllers */
-//angular.module('tweeplus', ['tweeplus.services', 'tweeplus.controllers']);
+angular.module('tweeplus',['ngResource','tweeplus.services', 'tweeplus.controllers', 'tweeplus.directives', 'tweeplus.filters']);
 
 /** services module initialization, allows adding services to module in multiple files */
-//angular.module('tweeplus.services', []);
-var app = angular.module('tweeplus',['ngResource']);
+angular.module('tweeplus.services', []);
